6efdd724d875dfb4e8a9e2d0680804cccee46a44
Sanjay-acharya-au50 <111633827+Sanjay-acharya-au50@users.noreply.github.com>
2023-03-09 01:10:03 +0530

    rend 2 log added

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ const app = express();
     app.get("/rend",(req,res)=>{
         res.send("hello render 2")
     })
+    app.get("/log",(req,res)=>{
+        res.send("hello render 2 log")
+    })
+
 
 app.listen(5003, ()=>{
     console.log("server running port 1000");
